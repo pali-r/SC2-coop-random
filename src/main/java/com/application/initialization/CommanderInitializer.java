70a2bab51b1f7c7a1f03214ed878c2331a4fd3c8
Pali <pavol.rindos@gmail.com>
2024-07-27 20:58:53 +0200

    CommanderInitializer rework

--- src/main/java/com/application/initialization/CommanderInitializer.java
+++ src/main/java/com/application/initialization/CommanderInitializer.java
@@ -5,9 +5,13 @@ import com.application.repository.CommanderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Component
 public class CommanderInitializer {
     private static CommanderRepo commanderRepo;
+    private final List<String> COMMANDER_NAMES = Arrays.asList("Raynor", "Kerrigan", "Artanis");
 
     @Autowired
     public CommanderInitializer(CommanderRepo commanderRepo) {
@@ -15,16 +19,12 @@ public class CommanderInitializer {
     }
 
     public void initializeCommanders() {
-        CommanderEntity commanderEntity = new CommanderEntity();
-        commanderEntity.setName("raynor");
-        commanderRepo.save(commanderEntity);
-
-        commanderEntity = new CommanderEntity();
-        commanderEntity.setName("kerrigan");
-        commanderRepo.save(commanderEntity);
-
-        commanderEntity = new CommanderEntity();
-        commanderEntity.setName("artanis");
-        commanderRepo.save(commanderEntity);
+        COMMANDER_NAMES.forEach(
+                commanderName -> {
+                    CommanderEntity commanderEntity = new CommanderEntity();
+                    commanderEntity.setName(commanderName);
+                    commanderRepo.save(commanderEntity);
+                }
+        );
     }
 }
